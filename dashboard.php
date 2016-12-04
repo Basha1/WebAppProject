bf7b0ab26679669f1e337c3e039f257291ed2eb3
jackbourkemckenna <jackbourkemckenna@gmail.com>
2016-12-04 09:42:50 -0500

    Videos working and only some imgur links

---
+++ dashboard.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<?php 
+
+//include nav bar from the nav.php file 
+include('nav.php');
+include('index.php');
+
+
+
+?>
+<html>
+<body>
+<div class="container">
+  
+  <h3 class = "Sub-Title">Top Post in /r/<?php echo $formvalue;?></h3>
+ <div class="col-xs-4"> 
+<div class="card card-inverse card-success comment-card">
+ <div class="card comment-card">   
+   <img class = "commentImg" src="imgs/comment.png">
+  <div class="card-block">
+    <h3 class =  "commentNum" ><?php echo $num_comments;?></h3>
+   
+
+   </div>
+   
+</div>
+   </div>
+      </div>
+   <div class="col-xs-4"> 
+<div class="card card-inverse card-success ups-card">
+ <div class="card ups-card"> 
+   <img class = "commentImg" src="imgs/up.png">  
+  <div class="card-block">
+   <h3 class =  "commentNum" ><?php echo $ups;?></h3>
+ 
+
+   </div>
+   
+</div>
+   </div>
+      </div>
+   <div class="col-xs-4"> 
+<div class="card card-inverse card-success user-card">
+ <div class="card user-card">
+  <img class = "commentImg" src="imgs/user.png">   
+  <div class="card-block">
+  
+
+      <h3 class =  "commentNum" ><?php echo $author;?></h3>
+
+   </div>
+   
+</div>
+   </div>
+      </div>
+  <div class="row">
+    <div class="col-xs-3 offset-xs-6">
+     <form action="dashboard.php" method="POST">
+<input class = "form-control" type="text" name="input_value">
+<input type="submit" name="submit" onClick="clearform()" id = "">
+</form>
+    </div>
+  </div>
+ <div class="jumbotron">
+  <h3 class="display-3"><?php echo $title?></h3>
+   <hr>
+   <?php echo $div;?>
+  
+  
+  <hr>
+    <a class="btn btn-primary btn-lg " href="<?php echo $url;?>" role="button">View Source</a>
+  <a class="btn btn-primary btn-lg" href="http://www.reddit.com<?php echo $comment_link;?>" role="button">View Comments</a>
+   </div>
+  
+
+  </div>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,22 @@
+
+   <link rel="stylesheet" href="style.css">
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>Reddit Stats</title>
+</head>
+
+<body>
+<nav class="navbar navbar-default" role="navigation">
+  <div class="navbar-header">
+    <a href="#" type="button" class="btn btn-default navbar-btn">
+      <span class="glyphicon glyphicon-home"></span>Home
+    </a>
+    
+    <a href="#" type="button" class="btn btn-default navbar-btn pull-right">
+      <span class="glyphicon glyphicon-shopping-cart"></span>Logout
+    </a>
+  </div>
+</nav>
