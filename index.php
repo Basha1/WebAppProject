961e171bb5c6d8d2f4dd5fe2bf528f4019a9349b
Basha1 <smahamed8@hotmail.com>
2016-12-06 09:16:42 +0000

    TwitterSearchHasTagApi
    done but needs styling!

--- index.php
+++ index.php
@@ -1,55 +1,52 @@
-
+<?php include "library/twitteroauth.php"; ?>
 
 <?php
-$div;
-if (isset($_POST['submit']))
-  {
-  // Execute this code if the submit button is pressed.
-  $formvalue = $_POST['input_value'];
+$consumer = "uPLkXrURWiWmNw34r6XrDkO3H";
+$consumersecret = "ZEet9hmyVjwUvfwmjuhV7Bcbmoi0HGowKQBEcsx0SINRLXHQrW";
+$accestoken = "804798848925319168-X74yBAHBeYFcAhNdRDBUj2PLi32IzV2";
+$accestokensecret = "gLY3ze8itfapdDJRK7TWfUeg1wFq6xUDQ3R567jmLbkG8";
+
+$twitter = new TwitterOAuth($consumer,$consumersecret,$accestoken, $accestokensecret);
+
+?>  
+
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Trendzzy</title>
+    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  </head>
+  <body>
+    <div class="main" >
+      <div id= "search">
+        
+
+    <h1> Search popular #hashtags  from twitter </h1>
+    <form action="" method="post">
+      <label>Search:<input type="text" name="keyword"/></label>
+    </form>
+              </div>
+    
    
-  }
-if($_POST == true) {
-$string_reddit = file_get_contents("https://www.reddit.com/r/$formvalue/.json?Count=1");
-$json = json_decode($string_reddit, true);  
-$jsonLoop = $json['data']['children'];
-foreach ($jsonLoop as $loop){
-    $title = $loop['data']['title'];
-    $url = $loop['data']['url'];
-		$num_comments = $loop['data']['num_comments'];
-		$ups = $loop['data']['ups'];
-		$author = $loop['data']['author'];
-		$comment_link = $loop['data']['permalink'];
-		$thumbnail = $loop['data']['thumbnail'];
-	
-	
-}
-	
-	
-
-$headers = get_headers( $url );
-
-$image_exist = implode(',',$headers);
-if ($div = strpos($image_exist, 'image') !== false) 
-{
-	
-   
-$div = '<img src="'.$url.'" class="img-fluid imgLink" alt="">';
-
-	 
-  
-}
-// will check if the the url is a vimeo link or a youtube link
-elseif ($div = preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $url, $match)) {
-    $video_id = $match[1];
-// I used this thread as help  http://stackoverflow.com/questions/2936467/parse-youtube-video-id-using-preg-match for help "' . ${myOb->content} . '"
-	$videoScr = "http://www.youtube.com/embed/$video_id";
-
-$div = '<html><iframe class ="youtubeEmbed"  frameborder="0" allowfullscreen src='.$videoScr.'></iframe></'; 
-    } 
-else 
-
-$div = '<img id="imgLink" src="'.$url.'"  alt="">';
-$divEror = '<a class ="failedDisplay"  href='.$url.'>If you are having trouble seeing content please click this link to view the source</a>';
-  //Opening it again to continue the else statement 
-		
-    }//main if to check if user has entered input 
+      <div id= "main-con">
+    <?php
+    
+    if( isset($_POST['keyword'])){
+      $tweets = $twitter->get('https://api.twitter.com/1.1/search/tweets.json?q='.$_POST['keyword'].'&result_type=popular&count=20');
+      foreach($tweets->statuses as $key => $tweet){
+        echo '<img src="'.$tweet->user->profile_image_url.'"/>' .$tweet->text.'<br>.';
+      }
+    }
+    ?>
+           </div>
+<footer>
+  <div class="Footer">
+  <p>
+    &copy; 2015 | <a href="https://github.com/Basha1">M.Basha</a> 
+    </p> 
+  </div>
+</footer>
+    </div>
+  </body>
+</html>
