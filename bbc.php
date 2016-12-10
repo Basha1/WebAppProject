983c91f517e93caa1997ca8bd45c883e0a1c9ff8
amysiggins10 <amysiggins10@gmail.com>
2016-12-10 11:34:56 -0500

    added bbc page and bbc api call script

---
+++ bbc.php
@@ -0,0 +1,142 @@
+<?php
+include('nav.php');
+include('bbc/bbcApicall.php');
+
+?>
+    
+        <div class="col-md-9">
+          <!-- Right -->
+
+          <strong><span class="glyphicon glyphicon-dashboard"></span>News Stroy</strong>
+
+
+          <hr>
+
+          <div class="row">
+            <div class="col-md-6">
+              <div class="well">Saved Links <span class="badge pull-right">Echo saved links</span></div>
+
+
+
+                 <!-- output from bbc.php-->
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h2 class="Sub-Title">Top News Stroy: <h3 class = "center-text"><?php echo $title;?></h3></h2></div>
+            
+                <?php// echo $div;?>
+                
+              
+
+                <hr>     
+                <a class="btn btn-primary btn-lg " href="<?php echo $url;?>" role="button">View Source</a>
+                <a class="btn btn-primary btn-lg" href="http://www.reddit.com<?php echo $comment_link;?>" role="button">View Comments</a>
+
+
+
+              </div>
+              <!--/panel-->
+
+
+              
+              <!--/panel-->
+
+            </div>
+            
+              <div class = "col-md-6">
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <div class="panel-title">
+                    <i class="glyphicon glyphicon-search"></i>
+                     Reddit search
+                  </div>
+                </div>
+                <div class="panel-body">
+
+                  <form class="form form-vertical" action="Reddit.php" method="POST">
+
+                    <div class="control-group">
+                      <label>Enter a subreddit</label>
+                      <div class="controls">
+                        <input type="text" class="form-control" placeholder="Enter Name" name="input_value">
+                      </div>
+                    </div>
+
+
+
+                    <div class="control-group">
+                      <label></label>
+                      <div class="controls">
+                        <input type="submit" class="btn btn-primary" name="submit" onClick="clearform()" id=""> Post
+                        </input>
+                      </div>
+                    </div>
+
+                  </form>
+
+
+                </div>
+                <!--/panel content-->
+              </div>
+              <!--/panel-->
+
+              <div class="panel panel-default">
+                <div class="panel-heading">
+                  <div class="panel-title">Stats from thread</div>
+                </div>
+                <div class="panel-body pull-center">
+
+                 <h3 class = "glyphicon glyphicon-user" ><?php echo $author;?></h3><hr>
+                   <a class="btn btn-primary btn-lg" action = "savedLinks.php" role="button">Save link</a>
+         
+
+                 
+
+                </div>
+              </div>
+              <!--/panel-->
+
+
+              <i class="icon-bar-chart icon-3x"></i>
+              <i class="icon-plus icon-3x"></i>
+              <i class="icon-facebook icon-3x"></i>
+              <i class="icon-beaker icon-3x"></i>
+              <i class="icon-twitter icon-3x"></i>
+
+
+            </div>
+            <!--/col-span-6-->
+
+          </div>
+          <!--/row-->
+        </div>
+        <!--/col-span-9-->
+      </div>
+    </div>
+    <!-- /Main -->
+
+
+
+    <div class="modal" id="addWidgetModal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+            <h4 class="modal-title">Add Widget</h4>
+          </div>
+          <div class="modal-body">
+            <p>Add a widget stuff here..</p>
+          </div>
+          <div class="modal-footer">
+            <a href="#" class="btn" data-dismiss="modal">Close</a>
+            <a href="#" class="btn btn-primary">Save changes</a>
+          </div>
+        </div>
+        <!-- /.modal-content -->
+      </div>
+      <!-- /.modal-dalog -->
+    </div>
+    <!-- /.modal -->
+<?php 
+include('footer.php');
+
+?>
---
+++ bbc/bbcApicall.php
@@ -0,0 +1,44 @@
+<?php
+include ('bbcKeys.php');
+
+
+$string_reddit = file_get_contents("https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=$apiKey");
+$json = json_decode($string_reddit, true);  
+$jsonLoop = $json['articles'];
+foreach ($jsonLoop as $loop){
+    $title = $loop['title'];
+    $discription = $loop['description'];
+    $url = $loop['url'];
+    $urlToImage = $loop['urlToImage'];
+    $publishDate =$loop['publishedAt'];
+    $author = $loop['author'];
+
+   
+    
+ 
+}
+echo $urlToImage;
+/* 
+The call to get the image link we are using in the reddit API call would not work in this api call: 
+<-----------$div = '<img class="img-fluid imgLink" src="'.$url.'"  alt="">';------------------->
+
+
+
+we think bbc are blocking image calls to their website. So we had to go off and research a way around this problem. 
+We found a solution to our problem at this link http://stackoverflow.com/questions/724391/saving-image-from-php-url
+source: Zuul
+
+we are downloading the image and displaying it from our server insted of using the api to display it
+--------------------------------------REMINDER------------------------------------------------------
+set imgs folder permishions to 777
+*/ 
+$ch = curl_init($urlToImage);
+$fp = fopen('imgs/bbcImg.jpg', 'wb');
+curl_setopt($ch, CURLOPT_FILE, $fp);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_exec($ch);
+curl_close($ch);
+fclose($fp);
+
+?>
