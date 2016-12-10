ee286cd1a3a27a8d7c0b4a15a44af79f6ecf0d8f
glennk010 <glennk010@gmail.com>
2016-12-10 06:52:40 -0500

    fix css on index.php

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
 require('loginSession.php');
+if (isset($_POST['btn-login'])) {
+ 
+ $email = strip_tags($_POST['email']);
+ $password = strip_tags($_POST['password']);
+ 
+ $email = $DBcon->real_escape_string($email);
+ $password = $DBcon->real_escape_string($password);
+ 
+ $query = $DBcon->query("SELECT user_id, email, password FROM tbl_users WHERE email='$email'");
+ $row=$query->fetch_array();
+ 
+ $count = $query->num_rows; // if email/password are correct returns must be 1 row
+ 
+ if (password_verify($password, $row['password']) && $count==1) {
+  $_SESSION['userSession'] = $row['user_id'];
+  header("Location: home.php");
+ } else {
+  $msg = "<div class='alert alert-danger'>
+     <span class='glyphicon glyphicon-info-sign'></span> &nbsp; Invalid Username or Password !
+    </div>";
+ }
+ $DBcon->close();
+}
 ?>
 <head>
   <meta charset="UTF-8" />
@@ -50,8 +73,8 @@ require('loginSession.php');
             <button type="submit" class="btn btn-default" name="btn-login" id="btn-login">
       <span class="glyphicon glyphicon-log-in"></span> &nbsp; Sign In
    </button> 
-            
-            <a href="register.php" class="btn btn-default" style="float:right;">Sign Up Here</a>
+  
+            <a href="register.php" class="btn btn-default">Sign Up Here</a>
             
         </div>  
         
