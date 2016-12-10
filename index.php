d7cfecdace885ba94556aec7878b2bac4b857bb0
glennk010 <glennk010@gmail.com>
2016-12-10 04:49:34 -0500

    added particle background to login

--- index.php
+++ index.php
@@ -1,43 +1,26 @@
 <?php
 require('loginSession.php');
-
-if (isset($_POST['btn-login'])) {
- 
- $email = strip_tags($_POST['email']);
- $password = strip_tags($_POST['password']);
- 
- $email = $DBcon->real_escape_string($email);
- $password = $DBcon->real_escape_string($password);
- 
- $query = $DBcon->query("SELECT user_id, email, password FROM tbl_users WHERE email='$email'");
- $row=$query->fetch_array();
- 
- $count = $query->num_rows; // if email/password are correct returns must be 1 row
- 
- if (password_verify($password, $row['password']) && $count==1) {
-  $_SESSION['userSession'] = $row['user_id'];
-  header("Location: home.php");
- } else {
-  $msg = "<div class='alert alert-danger'>
-     <span class='glyphicon glyphicon-info-sign'></span> &nbsp; Invalid Username or Password !
-    </div>";
- }
- $DBcon->close();
-}
-
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta charset="UTF-8" />
+  
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Coding Cage - Login & Registration System</title>
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" media="screen">
 <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen"> 
 <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
+  <link rel="stylesheet" href="particleground/source/css/style.css" />
+  <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  <script type='text/javascript' src='particleground/jquery.particleground.js'></script>
+  <script type='text/javascript' src='particleground/source/js/particleConfig.js'></script>
+</head>
+
 <body>
 
-<div class="signin-form">
+<div id="particles">
+  <div id="intro">
+   <div class="signin-form">
 
  <div class="container">
      
@@ -46,7 +29,7 @@ if (isset($_POST['btn-login'])) {
       
         <h2 class="form-signin-heading">Sign In.</h2><hr />
         
-        <?php
+       <?php
   if(isset($msg)){
    echo $msg;
   }
@@ -79,6 +62,8 @@ if (isset($_POST['btn-login'])) {
     </div>
     
 </div>
+  </div>
+</div>
+
 
 </body>
-</html>
